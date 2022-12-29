5623e6e9ea075374a5eb8e80bafca88fbba32289
Sagnik Jana <sagnikjana2001@gmail.com>
2022-12-29 22:39:53 +0530

    Added logic to check for presence of 'http' at start of longurl

--- urlSchema.js
+++ urlSchema.js
@@ -1,57 +1,52 @@
 import mongoose from "mongoose";
 import shortid from "shortid";
-import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-dotenv.config()
+import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import url from "node:url";
 
-const {ROOT_URL} = process.env
+dotenv.config();
 
+const { ROOT_URL } = process.env;
 
 const urlSchema = mongoose.Schema({
-    longUrl:{
-        type: String,
-        required:true
-    },
-    shortUrl:{
-        type:String,
-        required:true
-    },
-    clickCount:{
-        type: Number,
-        default: 0
-    }
+  longUrl: {
+    type: String,
+    required: true,
+  },
+  shortUrl: {
+    type: String,
+    required: true,
+  },
+  clickCount: {
+    type: Number,
+    default: 0,
+  },
 });
 
+urlSchema.statics.findByLongUrl = async function (longUrl) {
+  let shorturl = await this.findOne({ longUrl: longUrl });
 
+  if (shorturl != null) return ROOT_URL + "/" + shorturl.shortUrl;
 
-urlSchema.statics.findByLongUrl = async function(longUrl){
-    
-    let shorturl = await this.findOne({'longUrl':longUrl});
-    
-    
+  const protocolExists = url.parse(longUrl).protocol;
 
-    if(shorturl != null) return (ROOT_URL + '/' + shorturl.shortUrl);
-    
-    shorturl = await this.create({'longUrl': longUrl,'shortUrl' : shortid.generate()});
+  shorturl = await this.create({
+    longUrl: protocolExists != null ? longUrl : "http://" + longUrl,
+    shortUrl: shortid.generate(),
+  });
 
-    await shorturl.save()
+  await shorturl.save();
 
-    return (ROOT_URL + '/' + shorturl.shortUrl);
-}
+  return ROOT_URL + "/" + shorturl.shortUrl;
+};
 
-urlSchema.statics.findByShortUrl = async function(shortUrl){
-    
-    const longurl = await this.findOne({'shortUrl' : shortUrl});
-    
-    if(longurl != null) return (longurl.longUrl);
+urlSchema.statics.findByShortUrl = async function (shortUrl) {
+  const longurl = await this.findOne({ shortUrl: shortUrl });
 
-    return ROOT_URL;
+  if (longurl != null) return longurl.longUrl;
 
-}
+  return ROOT_URL;
+};
 
-const Url = mongoose.model('Url',urlSchema);
+const Url = mongoose.model("Url", urlSchema);
 
-export default Url
-
-
-
-
+export default Url;
